a3aaf0d5b43cd9c6b5eef3c7d5671051a1907708
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2025-10-06 11:11:41 -0300

    iniciando repo

---
+++ Ejemplos_MAUI/Ejemplo1_HolaMundo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Ejemplo1_HolaMundo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Ejemplos_MAUI/Ejemplo1_HolaMundo/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Ejemplo1_HolaMundo")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Ejemplo1_HolaMundo.Pages")]
